add0e0aebd6238566377b626733cd1d0a0a6a3f9
scg-77 <soch.games@gmail.com>
2025-11-23 15:55:29 +0100

    Déplacé la fonction file_basename vers posixPx4Common.h et posixPx4Comment.cpp

--- platforms/posix/src/px4/common/main.cpp
+++ platforms/posix/src/px4/common/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <px4_platform_common/defines.h>
+#include "posixPx4Common.h"
 
 using namespace std;
 
@@ -15,10 +15,19 @@ int main(int argc, char **argv){
     cout << "PX4_SHELL_COMMAND_PREFIX=" << PX4_SHELL_COMMAND_PREFIX << endl;
     int path_length = 0;
     
-    std::string absolute_binary_path; // full path to the px4 binary being executed
+    string absolute_binary_path; // full path to the px4 binary being executed
 
     int returnValue = PX4_OK;
     cout << "returnValue=" << returnValue << endl;
 	int instance = 0;
 
+    if(argc>0) {
+        /* The executed binary name could start with a path, so strip it away */
+		const string full_binary_name = argv[0];
+        cout << "full_binary_name=" << full_binary_name ;
+        const string binary_name = file_basename(full_binary_name);
+        cout << "binary_name=" << binary_name ;        
+    }
+
 }
+
